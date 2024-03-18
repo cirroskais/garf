404c4af435538cfc2dbbca572138bc8f99a0c121
cirroskais <cirroskais@madhouselabs.net>
2024-03-18 14:54:07 -0400

    oh hell yeah

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
     import { authorize, setActivity } from "./lib/discord";
 
+    let output = {};
+
     authorize().then(async (auth) => {
         // we now have our authenticated context.
-        await setActivity();
+        output = await setActivity();
     });
 </script>
 
 <main>
-    <p>yo</p>
+    <p>garf 2: eletric boogaloo</p>
+    <tt>{JSON.stringify(output, null, 4)}</tt>
 </main>
